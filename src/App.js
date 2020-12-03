01b10659c918ca5e837baeb876748efdc908ec33
kyhong222 <kyhong222@naver.com>
2020-12-03 22:19:17 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import React from 'react'
+import axios, {post} from 'axios';
 import ContentCard from './Component/ContentCard';
 
 function App() {
+  const [selectedFile, setSelectedFile] = React.useState(null)
+
+  const handleFileInput = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handlePost = () => {
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    console.log(selectedFile);
+
+    const url = 'http://localhost:3000';
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    }
+
+    return post(url, formData, config);
+  }
+
   return (
+    <>
+    <div>
+      <input type='file' name='file' onChange={e => handleFileInput(e)}/>
+      <button type="button" onClick={handlePost}>
+        전송
+      </button>
+    </div>
     <div>
       <ContentCard/>
     </div>
+    
+    
+    </>
   );
 }
 
